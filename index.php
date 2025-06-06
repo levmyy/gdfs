0e488fbb08dc48757c0b4ca927db18b9016d4dcd
levmyy <106603525+levmyy@users.noreply.github.com>
2025-06-06 10:02:28 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" 
+    content="width=device-width, initial-scale=1.0">
+    <title>Игровой сервис клауд гейминга</title>
+    <link rel="stylesheet" href="/css/main.css">
+</head>
+</body>
+    <div class="wrapper">
+        <?php require_once "blocks/header.php"; ?>
+        <div class="hero container">
+            <div class="hero--info">
+                <h2>Игровой сервис клауд гейминга</h2>
+                <h1>Мы гарантируем, что вы сможете запустить любое приложение благодаря нашему сервису клауд гейминга, доступному по месячной подписке</h1>
+                <p>*требуется стабильное интернет соединение от 100Мбит*</p>
+                <button class="btn">Получить больше  деталей о подписке></button>
+            </div>
+            <img src="img/joystick.svg" alt="">
+        </div>
+    </div>
+    <div class="container trending">
+        <h3>Популярные игры</h3>
+
+            <div class="games">
+                <div class="block">
+                    <img src="/img/game1.png" alt="">
+                    <span><img src="/img/fire.svg" alt=""> 1521 Отслеживают</span>
+                </div>
+                <div class="block">
+                    <img src="/img/game2.png" alt="">
+                    <span><img src="/img/fire.svg" alt=""> 1261 Отслеживают</span>
+                </div>
+                <div class="block">
+                    <img src="/img/game3.png" alt="">
+                    <span><img src="/img/fire.svg" alt=""> 955 Отслеживают</span>
+                </div>
+                <div class="block">
+                    <img src="/img/game4.png" alt="">
+                    <span><img src="/img/fire.svg" alt=""> 231 Отслеживают</span>
+                </div>
+            </div>            
+ 
+    <div class="features">
+        <div class="container">
+            <h3>Мы поддерживаем такие платформы как</h3>
+            <div class="info">
+                <div class="block">
+                    <img src="/img/mobile.png" alt="">
+                    <img src="/img/arrow.png" alt="">
+                    <p>Мобильный гейминг</p>
+                <div class="block">
+                    <img src="/img/pc.png" alt="">
+                    <img src="/img/arrow.png" alt="">
+                    <p>ПК гейминг</p>
+                <div class="block">
+                    <img src="/img/ps4.png" alt="">
+                    <img src="/img/arrow.png" alt="">
+                    <p>PS4 гейминг</p>
+                <div class="block">
+                    <img src="/img/vr.png" alt="">
+                    <img src="/img/arrow.png" alt="">
+                    <p>VR гейминг</p>                    
+                </div>
+            </div>
+        <div>
+    </div>   
+<body>
+
+</html>
